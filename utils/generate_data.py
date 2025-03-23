0529e7f91f6b3a5c34273be6208982412d31a9ed
Vyacheslav Rudenko <globalnet2078@gmail.com>
2025-03-23 17:45:52 +0300

    Очистка ветки main: оставлены только .gitignore, README.md и requirements.txt

--- utils/generate_data.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# Вспомогательная функция для генерации email
-def generate_email():
-    return f"user_{random.randint(100, 999)}@yandex.ru"
-
-# Вспомогательная функция для генерации пароля
-def generate_password():
-    return f"pass{random.randint(100, 999)}"
